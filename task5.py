e279de0a77416aa6319367153ead851c8ecbef4a
dszuflicki454 <d.szuflicki1@universityofgalway.ie>
2024-10-09 16:12:46 +0100

    5: Add names to csv file

--- task5.py
+++ task5.py
@@ -1,2 +1,17 @@
 import csv
 
+names = []
+last_input = input("Enter a name: ")
+
+while last_input != "quit":
+    names.append(last_input)
+    last_input = input("Enter a name: ")
+
+with open("task5.csv", 'w') as csv_file:
+    writer = csv.writer(csv_file)
+    [writer.writerow([name]) for name in names]
+    
+with open("task5.csv", 'r') as csv_file:
+    reader = csv.reader(csv_file)
+    for row in reader:
+        print(row[0]) # index the first element of the row (name) 
