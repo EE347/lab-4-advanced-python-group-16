f345d32194c50fd890cc759f3d052f8eae948833
dszuflicki454 <d.szuflicki1@universityofgalway.ie>
2024-10-09 16:14:03 +0100

    10: TIdy up graph from task 9

--- task10.py
+++ task10.py
@@ -1,3 +1,41 @@
 import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
+import numpy as np
+
+ys_dict = {}
+xs = []
+xlabel = None
+header = None
+
+with open("task9.csv", 'r') as csv_file:
+    reader = csv.reader(csv_file)
+    header = reader.__next__()
+
+    xlabel = header[0]
+    
+    for label in header[1:]:
+        ys_dict[label] = []
+    
+    for row in reader:
+        date = datetime.strptime(row[0], "%d/%m/%Y")
+        xs.append(date)
+
+        for i in range(1,len(header)):
+            ys_dict[header[i]].append(float(row[i]))
+
+color_dict = {"META":"b","AAPL":"gray","AMZN":"black","NFLX":"r","NVDA":"g","GOOGL":"orange"}
+
+
+[plt.plot(xs, y_line, color=color_dict[label]) for label, y_line in ys_dict.items()]
+plt.title("FAANNG Performance")
+plt.xlabel("Date")
+plt.ylabel("Price (USD)")
+
+labels = [label + ": +" + str(
+    round((ys_dict[label][-1]/ys_dict[label][0])*100,2)
+    ) + "%"
+    for label in header[1:]]
+
+plt.legend(labels)
+plt.show()
