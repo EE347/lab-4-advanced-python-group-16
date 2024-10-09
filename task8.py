b0275d67ee56db6910b5300836ccfed617c8ccd4
dszuflicki454 <d.szuflicki1@universityofgalway.ie>
2024-10-09 16:13:32 +0100

    8: Load and plot sin and cos

--- task8.py
+++ task8.py
@@ -1,2 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+x1, sin = np.load("task7_sin.npy")
+x2, cos = np.load("task7_cos.npy")
+
+plt.plot(x1, sin)
+plt.plot(x2, cos)
+plt.legend(["sin(x)","cos(x)"])
+plt.show()
